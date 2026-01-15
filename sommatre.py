14d73242904202b92171a1c690f85dfba0e7e22c
andrebesu <andrea.besusso@gmail.com>
2026-01-15 16:08:05 +0100

    Aggiunto esercizio 1

---
+++ sommatre.py
@@ -0,0 +1,13 @@
+#Programma che somma tre numeri(commento in linea)
+
+'''
+Commento su
+più righe
+che continua
+'''
+
+a = int(input("Inserisci un numero: "))
+b = int(input("Inserisci un numero: "))
+c = int(input("Inserisci un numero: "))
+somma = a+b+c
+print("La somma vale", somma)
