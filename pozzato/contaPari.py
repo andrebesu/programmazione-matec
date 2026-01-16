26fc8bb068321c7dbaca640bb767b52cbf857e51
andrebesu <andrea.besusso@gmail.com>
2026-01-16 16:31:16 +0100

    aggiunto esercizio a casa per 16/01

--- pozzato/contaPari.py
+++ pozzato/contaPari.py
@@ -2,3 +2,15 @@
 Programma che chiede all'utente di inserire n numeri interi, con n a sua volta
 in input, e conta quanti numeri pari sono stati inseriti
 '''
+
+
+n = int(input("Inserisci quanti interi vuoi utilizzare: "))
+i = 0
+conta_pari = 0
+while i < n:
+    a = int(input("Inserisci un intero: "))
+    resto = a % 2
+    if resto == 0:
+        conta_pari += 1
+        i = i + 1
+print("Sono stati inseriti", conta_pari, "numeri pari")
